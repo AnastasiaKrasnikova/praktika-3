d44d0b4e1dfeda6afafae29881e567b4e4dbd0e2
AnastasiaKrasnikova <gorypsiss@yandex.ru>
2025-05-29 15:01:01 +0300

    Update ConsoleView.java

--- ConsoleView.java
+++ ConsoleView.java
@@ -23,3 +23,20 @@ public class ConsoleView {
     public int getMenuChoice() {
         System.out.print("Выберите пункт меню: ");
         return Integer.
+            parseInt(scanner.nextLine());
+    }
+    
+    public String[] getVoterRegistrationData() {
+        System.out.print("ФИО: ");
+        String fullName = scanner.nextLine();
+        System.out.print("Дата рождения (ГГГГ-ММ-ДД): ");
+        LocalDate birthDate = LocalDate.parse(scanner.nextLine());
+        System.out.print("СНИЛС: ");
+        String snils = scanner.nextLine();
+        System.out.print("Логин: ");
+        String username = scanner.nextLine();
+        System.out.print("Пароль: ");
+        String password = scanner.nextLine();
+        return new String[]{username, password, fullName, birthDate.toString(), snils};
+    }
+}
